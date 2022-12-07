1976e2e10845b35d5509af556aa1e0c258e8ea59
samuelconlon <conlonsp@yahoo.com>
2022-12-07 15:39:09 -0500

    commit tree 9ab654829dfc7819cd3eb1f2904698803db2bece

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli == []
+    puts "The line is currently empty."
+  else
+    deli_line = []
+    x = 1
+    katz_deli.each do |e|
+      customer = "#{x}. #{e}"
+      deli_line << customer
+      x += 1
+    end
+    puts "The line is currently: #{deli_line.join(' ')}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.find_index(name) + 1} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli == []
+    puts "There is nobody waiting to be served!"
+  else
+    serving = katz_deli.shift
+    puts "Currently serving #{serving}."
+  end
+end
